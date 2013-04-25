ae758604694a3c29bc9ef64146754732257eba65
ahh2131 <ahh2131@columbia.edu>
2013-04-25 14:47:52 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+#***********************************************
+#main.py
+#Percolation module main function
+#Finds if a given or generated array percolates
+#Andreas Hadjigeorgiou - ahh2131
+#Assignment 4 
+#***********************************************
+
+import percolation_v2
+
+percolation_v2.main()
